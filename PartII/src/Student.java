cdf4407438b4b7d4439d9d2d199a6c2404cc263c
David Manouchehri <david@davidmanouchehri.com>
2015-08-06 22:17:00 -0400

    Passes tests, done!

--- PartII/src/Student.java
+++ PartII/src/Student.java
@@ -3,6 +3,29 @@
  *         Created on 8/6/15 at 7:18 PM.
  *         All content is under the MIT License unless otherwise specified.
  *         See LICENSE.txt for details.
+ *
+ *         Page 407, P8.7
  */
 public class Student {
+    private String name;
+    private double totalScore = 0;
+    private int quizCount = 0;
+
+    public Student(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getTotalScore() {
+        return totalScore;
+    }
+    public double getAverageScore() {
+        return totalScore / quizCount;
+    }
+
+    public void addQuiz(double score) {
+        this.quizCount++;
+        this.totalScore += score;
+    }
 }
--- PartII/src/StudentTester.java
+++ PartII/src/StudentTester.java
@@ -4,10 +4,15 @@
  *         All content is under the MIT License unless otherwise specified.
  *         See LICENSE.txt for details.
  *
- *         Page 407
+ *         Page 407, P8.7
  */
 public class StudentTester {
     public static void main(String[] args) {
-
+        Student Tommy = new Student("Tommy");
+        System.out.println(Tommy.getName());
+        Tommy.addQuiz(85.00);
+        Tommy.addQuiz(95.00);
+        System.out.println(Tommy.getTotalScore());
+        System.out.println(Tommy.getAverageScore());
     }
 }
